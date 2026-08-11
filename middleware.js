7957ae639474bb29d85800483f13d0a742f48387
LAPASSIIOON <karimssaleh52@gmail.com>
2026-08-11 14:19:28 +0300

    حذف الملفات القديمة المكررة: middleware في الجذر + ArcMark

--- middleware.js
+++
@@ -1,9 +0,0 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from './src/i18n/routing.js';
-
-export default createMiddleware(routing);
-
-export const config = {
-  // يستثني ملفات API والأصول الثابتة وملفات Next الداخلية
-  matcher: ['/((?!api|_next|.*\\..*).*)'],
-};
